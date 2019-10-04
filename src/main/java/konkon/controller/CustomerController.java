189e4467a8695df6fdd4f8a6623cac5a22112744
akityty <congnv.9803@gmail.com>
2019-10-04 11:09:01 +0700

    them phan trang, tim theo ten

--- src/main/java/konkon/controller/CustomerController.java
+++ src/main/java/konkon/controller/CustomerController.java
@@ -6,14 +6,13 @@ import konkon.model.Province;
 import konkon.service.CustomerService;
 import konkon.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.jws.WebParam;
+import java.util.Optional;
 
 @Controller
 public class CustomerController {
@@ -30,8 +29,14 @@ public class CustomerController {
     return provinceService.findAll();
   }
   @GetMapping("/customers")
-  public ModelAndView showCustomers(){
-    Iterable<Customer> customers = customerService.findAll();
+  public ModelAndView showCustomers(@RequestParam("s") Optional<String> s, Pageable pageable){
+   /* Page<Customer> customers = customerService.findAll(pageable);*/
+    Page<Customer> customers;
+    if(s.isPresent()){
+      customers = customerService.findAllByFirstNameContaining(s.get(), pageable);
+    } else {
+      customers = customerService.findAll(pageable);
+    }
     ModelAndView modelAndView = new ModelAndView("/customer/list");
     modelAndView.addObject("customers", customers);
     return modelAndView;
--- src/main/java/konkon/service/impl/CustomerServiceImpl.java
+++ src/main/java/konkon/service/impl/CustomerServiceImpl.java
@@ -5,13 +5,17 @@ import konkon.model.Province;
 import konkon.repository.CustomerRepository;
 import konkon.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public class CustomerServiceImpl implements CustomerService {
   @Autowired
   private CustomerRepository customerRepository;
+
+
   @Override
-  public Iterable<Customer> findAll() {
-    return customerRepository.findAll();
+  public Page<Customer> findAll(Pageable pageable) {
+    return customerRepository.findAll(pageable);
   }
 
   @Override
@@ -32,5 +36,10 @@ public class CustomerServiceImpl implements CustomerService {
   @Override
   public Iterable<Customer> findAllByProvince(Province province) {
     return customerRepository.findAllByProvince(province);
+  }
+
+  @Override
+  public Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable) {
+    return customerRepository.findAllByFirstNameContaining(firstname,pageable);
   }
 }
